dc689c54aeb8cb6e2e878a32d44ed6bbd30c16fd
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-10-16 16:06:45 +0500

    .

--- src/modules/categories/categories.schema.ts
+++ src/modules/categories/categories.schema.ts
@@ -1,6 +1,5 @@
 import { Document, Types } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
 export type CategoriesDocument = Categories & Document;
 
 @Schema({ collection: 'categories' })
