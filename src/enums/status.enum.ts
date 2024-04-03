0dd6727629e820ee1466f5ac609556adad5533af
Muhiddin <muhiddinjv@gmail.com>
2024-04-03 11:45:23 +0500

    delete all

--- src/enums/status.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum Status {
-  Pending = 0,
-  Rejected = -1,
-  Approved = 1,
-}
--- src/modules/auth/constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-    secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
-}; //TODO: Use secrets vault, environment variable, or configuration service isntead of this value
