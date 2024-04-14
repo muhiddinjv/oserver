15573cf0c7932f304c8a95531b44d8d51f5b2f67
Muhiddin <muhiddinjv@gmail.com>
2024-04-14 15:45:13 +0500

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,8 @@ import { CatalogsModule } from './modules/catalog/catalog.module';
 import { GoodsModule } from './modules/goods/goods.module';
 import { BillsModule } from './modules/bills/bills.module';
 import { AuthGuard } from './modules/auth/auth.guard';
+import { TransfersModule } from './modules/transfers/transfers.module';
 import { APP_GUARD } from '@nestjs/core';
-import { TransfersModule } from './transfers/transfers.module';
 
 @Module({
   imports: [
